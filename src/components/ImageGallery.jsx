f4d097b670a70a70969034deb1fbd56087d278b6
paulromer12 <paulromer2036@gmail.com>
2025-05-21 23:07:27 -0500

    Updated image rotation

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -5,7 +5,7 @@ import img3 from '../assets/homepage/racing.jpeg';
 const images = [
   { src: img1, initialRotation: '-2deg' }, // Storing the rotation value
   { src: img2, initialRotation: '1.5deg' },
-  { src: img3, initialRotation: '-1deg' },
+  { src: img3, initialRotation: '-1.25deg' },
 ];
 
 export default function ImageGallery() {
