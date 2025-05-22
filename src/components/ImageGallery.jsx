efefe6531a13e60c2f45e19093e6b3d4db537148
paulromer12 <paulromer2036@gmail.com>
2025-05-21 22:51:45 -0500

    Added rotation to images

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,13 +2,25 @@ import img1 from '../assets/homepage/pumpkins.jpeg';
 import img2 from '../assets/homepage/me.jpeg';
 import img3 from '../assets/homepage/racing.jpeg';
 
-const images = [img1, img2, img3];
+const images = [
+  { src: img1, initialRotation: '-2deg' }, // Storing the rotation value
+  { src: img2, initialRotation: '1.5deg' },
+  { src: img3, initialRotation: '-1deg' },
+];
 
 export default function ImageGallery() {
   return (
     <div className="gallery">
-      {images.map((src, i) => (
-        <img key={i} src={src} alt={`Paul Romer ${i + 1}`} className="avatar" />
+      {images.map((image, i) => (
+        <img
+          key={i}
+          src={image.src}
+          alt={`Paul Romer ${i + 1}`}
+          className="avatar"
+          style={{
+            '--initial-rotation': image.initialRotation // Set a CSS custom property
+          }}
+        />
       ))}
     </div>
   );
